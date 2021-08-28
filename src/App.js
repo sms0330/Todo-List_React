2ec1b7eb8675e3b919fe550da42a9ac7f719cf5d
Joseph Son <sms0330@gmail.com>
2021-08-27 19:17:36 -0700

    Added Handle Remove

--- src/App.js
+++ src/App.js
@@ -63,13 +63,21 @@ class App extends Component {
     });
   }
 
+  handleRemove = (id) => {
+    const { todos } = this.state;
+    this.setState({
+      todos: todos.filter(todo => todo.id !== id)
+    });
+  }
+
   render() {
     const { input, todos } = this.state;
     const {
       handleChange,
       handleCreate,
       handleKeyPress,
-      handleToggle
+      handleToggle,
+      handleRemove
     } = this;
 
     return (
@@ -81,7 +89,7 @@ class App extends Component {
           onCreate={handleCreate}
         />
       )}>
-        <TodoItemList todos={todos} onToggle={handleToggle}/>
+        <TodoItemList todos={todos} onToggle={handleToggle} onRemove={handleRemove}/>
       </TodoListTemplate>
     );
   }
